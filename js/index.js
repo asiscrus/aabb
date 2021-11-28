85912302ad620833edd1b26de95055d42bde01fb
Murat Y <42816772+asiscrus@users.noreply.github.com>
2021-11-28 20:27:56 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import '../css/fontawesome-all.min.css';
+import '../css/bootstrap.min.css';
+import '../css/magnific-popup.css';
+import '../slick/slick.css';
+import '../slick/slick-theme.css';
+import '../css/tooplate-style.css';
+import './jquery-3.2.1.min.js';
+import './jquery.magnific-popup.min.js';
+import './jquery.backstretch.min.js';
+import '../slick/slick.min.js';
+
